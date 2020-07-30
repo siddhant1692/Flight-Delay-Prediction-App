017c35d28d35db0c0c43b9b8034686f663a8f7d0
Siddhant Sawant <61906348+siddhant1692@users.noreply.github.com>
2020-07-30 15:40:41 +0530

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shinydashboard", "shiny", "caret", "xgboost", "methods")
+my_packages = c("shinydashboard", "shiny", "caret", "xgboost", "methods","data.table")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
